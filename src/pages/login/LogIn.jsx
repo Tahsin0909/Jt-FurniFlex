0299a6e278a898739ff696bdde93f0a347f78146
Tahsin Zaman <tajisan789@gmail.com>
2024-09-06 00:06:52 +0600

    Added dependicies and paths

---
+++ src/pages/login/LogIn.jsx
@@ -0,0 +1,10 @@
+
+const LogIn = () => {
+    return (
+        <div>
+            <p>Login</p>
+        </div>
+    );
+};
+
+export default LogIn;
---
+++ src/router/Router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import LogIn from "../pages/login/LogIn";
+import SignUp from "../pages/signup/SignUp";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      errorElement: <p>Error ...</p>,
+      children:[
+        {
+            path:'/',
+            element: <p>Home</p>
+        },
+        {
+            path:'/logIn',
+            element: <LogIn/>
+        },
+        {
+            path:'/SignUp',
+            element: <SignUp/>
+        }
+      ]
+    },
+  ]);
