c35cdbd4af9077e195de7d6776319a9de8660472
Tahsin Zaman <tajisan789@gmail.com>
2024-09-07 19:26:49 +0600

    Added skeleton loader in ordercart

--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -120,6 +120,28 @@ const Order = () => {
                                 <p className="text-end font-bold">$299.00</p>
                             </div>
                         </div>
+                        <div className="border-b pb-padding_md animate-pulse">
+    <div className="flex items-start gap-gap_base p-padding_small">
+        {/* Empty Space for Item Quantity */}
+        <div className="w-[88px] h-[44px]"></div>
+
+        {/* Image Skeleton */}
+        <div className="w-[88px] h-[88px] bg-gray-300 rounded-rounded_primary"></div>
+
+        {/* Product Name Skeleton */}
+        <div className="flex-1">
+            <div className="bg-gray-300 h-5 w-2/3 mb-2 rounded"></div>
+        </div>
+    </div>
+
+    {/* Price Skeleton */}
+    <div className="h-5 w-1/4 bg-gray-300 rounded mt-2 ml-auto pr-padding_small"></div>
+</div>
+
+
+
+
+
                     </div>
                 </div>
                 <div className="lg:col-span-3 md:col-span-2">
